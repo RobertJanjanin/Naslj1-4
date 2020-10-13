67f72686b25b5fd5d65f3ea9462c882ef104f248
RobertJanjanin <55134583+RobertJanjanin@users.noreply.github.com>
2020-10-13 11:22:41 +0200

    Add files via upload

---
+++ robertJanjaninNaslj1-4/robertJanjaninNaslj1-4/Program.cs
@@ -0,0 +1,57 @@
+﻿using System;
+
+
+namespace robertJanjaninNaslj1_4
+{
+   
+    class Program
+    {
+        interface IControl
+        {
+            void Metoda();
+            int Brojac { get; set; }
+
+        }
+        class ListBox : IControl
+        {
+            int brojac;
+            public int Brojac
+            {
+                get { return brojac; }
+                set { brojac = value; }
+            }
+
+            public void Metoda()
+            {
+                Console.WriteLine("Metoda List");
+            }
+        }
+
+        static void Main(string[] args)
+        {
+            ListBox X = new ListBox();
+            X.Metoda();
+            Console.ReadKey();
+
+        }
+        class ComboBox : IControl {
+            int brojac;
+            public int Brojac
+            {
+                get { return brojac; }
+                set { brojac = value; }
+            }
+
+            public void Metoda()
+            {
+                Console.WriteLine("Metoda");
+            }
+        }
+ 
+
+
+
+    }
+}
+ 
+
